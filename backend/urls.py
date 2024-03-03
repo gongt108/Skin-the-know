9fde8682e7481e95dda1992383365bfdaa3341cd
Tiffany Gong <gongt108@gmail.com>
2024-03-02 22:48:00 -0800

    set up authentication backend

--- backend/urls.py
+++ backend/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hello-world/", views.hello_world, name='hello_world'),
+    path("hello-world/", views.hello_world, name="hello_world"),
+    path("login/", views.login_view, name="api-login"),
+    path("logout/", views.logout_view, name="api-logout"),
+    path("session/", views.session_view, name="api-session"),
+    path("whoami/", views.whoami_view, name="api-whoami"),
 ]
