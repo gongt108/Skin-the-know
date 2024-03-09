0fe0388fc1113d44dea56085ff57bf5ce17a8e85
Tiffany Gong <gongt108@gmail.com>
2024-03-08 20:57:39 -0800

    fix tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Cookies from 'universal-cookie';
 import axios from 'axios';
 import Layout from './components/Layout';
 import Login from './components/auth/Login';
+import './input.css';
 
 //instantiating Cookies class by creating cookies object
 const cookies = new Cookies();
@@ -12,6 +13,7 @@ const App = () => {
 		<Layout>
 			<Login />
 		</Layout>
+		// <h1 className="text-3xl font-bold underline">Hello world!</h1>
 	);
 };
 
