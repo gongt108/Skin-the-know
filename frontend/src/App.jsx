a56c2fe984e1cb861dc323f0502947a336a4c5cc
Tiffany Gong <gongt108@gmail.com>
2024-03-09 18:23:31 -0800

    install react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 import Layout from './components/Layout';
@@ -10,9 +11,15 @@ const cookies = new Cookies();
 
 const App = () => {
 	return (
-		<Layout>
-			<Login />
-		</Layout>
+		<Router>
+			<Layout>
+				<Routes>
+					{/* <Route path="/" exact component={Home} /> */}
+					<Route path="/login" element={<Login />} />
+					{/* <Route path="/signup" component={Signup} /> */}
+				</Routes>
+			</Layout>
+		</Router>
 		// <h1 className="text-3xl font-bold underline">Hello world!</h1>
 	);
 };
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,19 +5,11 @@ import Navbar from './Navbar';
 const Layout = ({ children }) => {
 	return (
 		<div>
-			<header>
-				{/* Header content goes here */}
-				<h1>My App</h1>
-			</header>
 			<Navbar />
 			<main>
 				{/* Main content (usually page-specific content) */}
 				{children}
 			</main>
-			<footer>
-				{/* Footer content goes here */}
-				<p>&copy; 2024 My App</p>
-			</footer>
 		</div>
 	);
 };
