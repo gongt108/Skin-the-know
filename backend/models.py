147c2473121341e3a7f72b1a02594e0f788131ee
Tiffany Gong <gongt108@gmail.com>
2024-03-14 01:14:53 -0700

    add Ingredient, Brand, Product, SkinConcern models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    good_list = models.ManyToManyField(
+        "self", symmetrical=False, related_name="good_for", blank=True
+    )
+    avoid_list = models.ManyToManyField(
+        "self", symmetrical=False, related_name="to_avoid", blank=True
+    )
+    caution_list = models.ManyToManyField(
+        "self", symmetrical=False, related_name="use_with_caution", blank=True
+    )
+    incidecoder_url = models.URLField(null=True)
+    img_url = models.URLField(null=True)
+    skin_concern = models.ManyToManyField("SkinConcern", blank=True)
+
+
+class Brand(models.Model):
+    name = models.CharField()
+    img_url = models.CharField(null=True)
+
+
+class Product(models.Model):
+    name = models.CharField()
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True)
+    ingredients = models.ManyToManyField(
+        Ingredient, on_delete=models.SET_NULL, null=True
+    )
+    main_active = models.ForeignKey(Ingredient, on_delete=models.SET_NULL, null=True)
+    skin_concern = models.ManyToManyField("SkinConcern", blank=True)
+    img_url = models.CharField(null=True)
+    incidecoder_url = models.CharField(null=True)
+
+
+class SkinConcern(models.Model):
+    name = models.CharField(max_length=100)
