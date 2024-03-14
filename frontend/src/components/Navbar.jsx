12572ad2bbcd084950a5fb171c40b9330467b6e7
Tiffany Gong <gongt108@gmail.com>
2024-03-13 22:55:34 -0700

    add drop down menu to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Dropdown } from 'flowbite-react';
 
 function Navbar() {
 	const [isSearching, setIsSearching] = useState(false);
@@ -8,135 +9,148 @@ function Navbar() {
 	};
 
 	return (
-		<nav className="flex flex-col bg-white border-gray-200 dark:bg-gray-900">
-			<div className="w-[60rem] flex flex-wrap items-center justify-between mx-auto p-4">
-				<a
-					href="https://flowbite.com/"
-					className="flex items-center space-x-3 rtl:space-x-reverse"
-				>
-					<img
-						src="https://flowbite.com/docs/images/logo.svg"
-						className="h-8"
-						alt="Flowbite Logo"
-					/>
-					<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-						Skin the Know
-					</span>
-				</a>
-				<div className="flex md:order-2">
-					<button
-						type="button"
-						data-collapse-toggle="navbar-search"
-						aria-controls="navbar-search"
-						aria-expanded="false"
-						className=" text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
-						onClick={toggleSearch}
+		<div className="flex flex-col">
+			<nav className="flex bg-white border-gray-200 dark:bg-gray-900">
+				<div className="w-[60rem] flex flex-wrap items-center justify-between mx-auto p-4">
+					<a
+						href="/"
+						className="flex items-center space-x-3 rtl:space-x-reverse"
 					>
-						<svg
-							className="w-5 h-5"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 20 20"
+						<img
+							src="https://flowbite.com/docs/images/logo.svg"
+							className="h-8"
+							alt="Flowbite Logo"
+						/>
+						<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+							Skin the Know
+						</span>
+					</a>
+					<div className="flex md:order-2">
+						<button
+							type="button"
+							data-collapse-toggle="navbar-search"
+							aria-controls="navbar-search"
+							aria-expanded="false"
+							className=" text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
+							onClick={toggleSearch}
 						>
-							<path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-							/>
-						</svg>
-						<span className="sr-only">Search</span>
-					</button>
+							<svg
+								className="w-5 h-5"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 20 20"
+							>
+								<path
+									stroke="currentColor"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+								/>
+							</svg>
+							<span className="sr-only">Search</span>
+						</button>
+						<button
+							data-collapse-toggle="navbar-search"
+							type="button"
+							className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+							aria-controls="navbar-search"
+							aria-expanded="false"
+						>
+							<span className="sr-only">Open main menu</span>
+							<svg
+								className="w-5 h-5"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 17 14"
+							>
+								<path
+									stroke="currentColor"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M1 1h15M1 7h15M1 13h15"
+								/>
+							</svg>
+						</button>
+					</div>
+					<div
+						className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+						id="navbar-search"
+					>
+						<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+							<li>
+								<a
+									href="#"
+									className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+									aria-current="page"
+								>
+									Home
+								</a>
+							</li>
+							<li>
+								<a
+									href="#"
+									className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+								>
+									Products
+								</a>
+							</li>
+							<li>
+								<a
+									href="#"
+									className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+								>
+									Ingredients
+								</a>
+							</li>
+							<li>
+								<a
+									href="#"
+									className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+								>
+									Brands
+								</a>
+							</li>
 
-					<button
-						data-collapse-toggle="navbar-search"
-						type="button"
-						className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-						aria-controls="navbar-search"
-						aria-expanded="false"
-					>
-						<span className="sr-only">Open main menu</span>
-						<svg
-							className="w-5 h-5"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 17 14"
-						>
-							<path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M1 1h15M1 7h15M1 13h15"
-							/>
-						</svg>
-					</button>
+							<li>
+								<Dropdown
+									inline
+									className="text-white white z-20"
+									dismissOnClick={false}
+									renderTrigger={() => (
+										<span className="text-white cursor-pointer hover:text-blue-500">
+											Skin Concerns
+										</span>
+									)}
+								>
+									<Dropdown.Item>Acne</Dropdown.Item>
+									<Dropdown.Item>Hyperpigmentation</Dropdown.Item>
+									<Dropdown.Item>Hydration</Dropdown.Item>
+									<Dropdown.Item>Oil Control</Dropdown.Item>
+									<Dropdown.Item>Pores</Dropdown.Item>
+									<Dropdown.Item>Anti-aging</Dropdown.Item>
+								</Dropdown>
+							</li>
+						</ul>
+					</div>
 				</div>
-				<div
-					className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-					id="navbar-search"
-				>
-					<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-						<li>
-							<a
-								href="#"
-								className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-								aria-current="page"
-							>
-								Home
-							</a>
-						</li>
-						<li>
-							<a
-								href="#"
-								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>
-								Products
-							</a>
-						</li>
-						<li>
-							<a
-								href="#"
-								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>
-								Ingredients
-							</a>
-						</li>
-						<li>
-							<a
-								href="#"
-								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>
-								Brands
-							</a>
-						</li>
-						<li>
-							<a
-								href="#"
-								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-							>
-								Skin Concern
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-			{/* <div
-				className={`w-[60rem]  flex-wrap items-center justify-between mx-auto p-4 ${
+			</nav>
+			<div
+				className={`max-w-[60rem] w-full items-center justify-between mx-auto ${
 					isSearching ? 'flex' : 'hidden'
 				} `}
 			>
 				<input
 					type="text"
 					id="search-navbar"
-					class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					class="block w-full py-4 p-2 ps-10 text-sm text-gray-900 bg-gray-300/50  focus:outline-none"
 					placeholder="Search..."
 				/>
-			</div> */}
-		</nav>
+			</div>
+		</div>
 	);
 }
 
