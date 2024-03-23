8491d71c8e1b115f9635684acdf38469c3c2f1e1
Tiffany Gong <gongt108@gmail.com>
2024-03-23 00:55:02 -0700

    create productspider

--- spider/spider/spiders/product_spider.py
+++ spider/spider/spiders/product_spider.py
@@ -10,18 +10,31 @@ class QuotesSpider(scrapy.Spider):
 
     custom_settings = {
         "FEEDS": {"booksdata.json": {"format": "json", "overwrite": True}},
-        "ITEM_PIPELINES": {
-            "spider.pipelines.IngredientListPipeline": 300,
-            "spider.pipelines.SaveToPostgresPipeline": 400,
-        },
+        # "ITEM_PIPELINES": {
+        # "spider.pipelines.IngredientListPipeline": 300,
+        # "spider.pipelines.SaveToPostgresPipeline": 400,
+        # },
     }
 
     def start_requests(self):
-        urls = ["https://incidecoder.com/products/benton-aloe-bha-skin-toner"]
+        urls = ["https://incidecoder.com/brands/some-by-mi"]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        products = response.css("div.paddingb60 a.klavika.simpletextlistitem")
+        for product in products:
+            relative_url = product.css("::attr(href)").get()
+            product_url = "https://incidecoder.com" + relative_url
+
+            yield response.follow(product_url, callback=self.parse_product_page)
+
+        next_page = response.css("div.center.fs16 a ::attr(href)").get()
+        if next_page is not None:
+            next_page_url = "https://incidecoder.com" + next_page
+            yield response.follow(next_page_url, callback=self.parse)
+
+    def parse_product_page(self, response):
         ingredient_item = IngredientListItem()
         ingredients = response.css("div#ingredlist-short").css("span")
         # print(ingredients)
