85738f581d52823a38e6e4f4ad40fe9ef42c6fd1
Tiffany Gong <gongt108@gmail.com>
2024-05-03 23:52:29 -0700

    add go back for packing list

--- frontend/src/PackingList.jsx
+++ frontend/src/PackingList.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { Dropdown, DropdownItem, Modal, Button } from 'flowbite-react';
+import { IoPrint } from 'react-icons/io5';
+import { IoIosArrowBack } from 'react-icons/io';
 
 function PackingList() {
 	const [isLoading, setIsLoading] = useState(true);
@@ -10,6 +12,7 @@ function PackingList() {
 	const [searchParams, setSearchParams] = useSearchParams();
 	const [name, setName] = useState('');
 	const week = searchParams.get('week_id');
+	const navigateTo = useNavigate();
 
 	useEffect(() => {
 		axios
@@ -33,9 +36,25 @@ function PackingList() {
 
 	return (
 		<div className="w-[60rem] mx-auto">
-			<h1 className="flex font-semibold text-2xl text-center my-8">
-				{name} Packing List
-			</h1>
+			<p
+				className="flex items-center mt-4 hover:underline hover:text-blue-500"
+				onClick={() => navigateTo(-1)}
+			>
+				<IoIosArrowBack size={18} className="me-2" /> Go Back
+			</p>
+			<div className="flex justify-between align-middle items-center">
+				<h1 className="flex font-semibold text-2xl text-center my-4">
+					{name} Packing List
+				</h1>
+				<p
+					onClick={() => {
+						window.print();
+					}}
+					className="me-4"
+				>
+					<IoPrint size={20} />
+				</p>
+			</div>
 			<table className="w-full">
 				<thead>
 					<tr>
